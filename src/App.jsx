31ba0ab3ecb6ac7a28db153a02d63d966ddc5410
DanielSousa001 <150301394+DanielSousa001@users.noreply.github.com>
2023-11-14 01:46:41 +0000

    nw

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,28 @@ import './App.css'
 import Card from './components/Card/Card';
 
 function App() {
-const item1 = {
-  name: 'Rick Sanchez',
-  imageUrl: "https://rickmortyapi.com/api/character/avatar/1.jpeg"
-}
+  const item1 = {
+    name: "Rick Sanchez",
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+  }
 
   const item2 = {
-    name: 'Morty Smith',
-    imageUrl: "https://rickmortyapi.com/api/character/avatar/2.jpeg"
-}
+    name: "Morty Smith",
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+  }
 
-const item3 = {
-  name: "Summer Smith",
-  imageUrl: "https://rickmortyapi.com/api/character/avatar/3.jpeg"
-}
+  const item3 = {
+    name: "Summer Smith",
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/3.jpeg"
+  }
 
-   const items = [item1, item2, item3]
+  const items = [item1, item2, item3]
 
   return (
     <>
-        {items.map(function (element) {
-           return <Card item={element} />
-        })}  
+      {items.map(function (element) {
+        return <Card item={element} />
+      })}
     </>
   )
 }
