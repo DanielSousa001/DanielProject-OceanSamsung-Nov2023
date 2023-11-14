0080b434504d89e51b03431ec751c4b54101ce17
DanielSousa001 <150301394+DanielSousa001@users.noreply.github.com>
2023-11-14 01:28:10 +0000

    implementacao dos cards

--- src/App.jsx
+++ src/App.jsx
@@ -17,11 +17,13 @@ const item3 = {
   imageUrl: "https://rickmortyapi.com/apicharacter/avatar/3.jpeg"
 }
 
+   const items = [item1, item2, item3]
+
   return (
     <>
-      <br />
-      <Card item={item1} />
-      <Card item={item2} />
+        {items.map(function (element) {
+           return <card item={element} />
+        })}  
     </>
   )
 }
