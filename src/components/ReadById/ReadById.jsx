30835aae56f7fc728335e0ccd2dbe6932c5561f5
DanielSousa001 <150301394+DanielSousa001@users.noreply.github.com>
2023-11-16 23:55:39 +0000

    Inserção do HTML da página

--- src/components/ReadById/ReadById.jsx
+++ src/components/ReadById/ReadById.jsx
@@ -14,18 +14,47 @@ export default function ReadById() {
     const body = await response.json()
 
     setItem(body)
-
-    console.log(body)
-
-    // TODO: Implementar o carregamento da API
-    // TODO: Guardar a informação recebida da API em um estado
   }
 
-  useEffect(function() {
+  useEffect(function () {
     carregarDadosApi()
   }, [])
 
   // TODO: Exibir essas informações no JSX
 
-  return <div>ReadById: {id} - {item.name}</div>
+  // return <div>ReadById: {id} - {item.name}</div>
+
+  return (
+    <div className="main">
+      <div className="image-wrapper">
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="Imagem do Personagem" />
+      </div>
+
+      <div className="info">
+        <div className="info-tag">Status: Alive</div>
+
+        <div className="info-title">Rick Sanchez</div>
+
+        <div className="info-row">
+          <div className="info-label">Species:</div>
+          <div className="info-text">Human</div>
+        </div>
+
+        <div className="info-row">
+          <div className="info-label">Gender:</div>
+          <div className="info-text">Male</div>
+        </div>
+
+        <div className="info-row">
+          <div className="info-label">Origin:</div>
+          <div className="info-text">Earth (C-137)</div>
+        </div>
+
+        <div className="info-row">
+          <div className="info-label">Location:</div>
+          <div className="info-text">Citadel of Ricks</div>
+        </div>
+      </div>
+    </div>
+  )
 }
